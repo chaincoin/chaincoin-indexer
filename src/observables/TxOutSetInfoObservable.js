e67ba85d9909b8c7750b0b6d447f544901257d54
chrismcna <chris.mcna@gmail.com>
2019-06-27 13:53:33 +0100

    Update TxOutSetInfoObservable.js

--- src/observables/TxOutSetInfoObservable.js
+++ src/observables/TxOutSetInfoObservable.js
@@ -4,25 +4,43 @@ const { shareReplay } = require('rxjs/operators');
 
 module.exports = function (chaincoinService) {
 
-  return Observable.create(function (observer) {
+};
 
-    var txOutSetInfo = null;
 
-    var getTxOutSetInfo = async () => {
-      var newTxOutSetInfo = await chaincoinService.chaincoinApi.getTxOutSetInfo();
+module.exports = function (chaincoinService) {
 
-      if (txOutSetInfo != null && newTxOutSetInfo.height == txOutSetInfo.bestblock && newTxOutSetInfo.height == txOutSetInfo.bestblock) return;
-      txOutSetInfo = newTxOutSetInfo;
-      observer.next(newTxOutSetInfo);
-    };
+  var txOutSetInfoObservableCache = []; //TODO: memory leak
 
-    var bestBlockHashSubscription = chaincoinService.BestBlockHash.subscribe(bestBlockHash => getTxOutSetInfo());
+  return (blockHash) => {
 
-    return () => {
-      bestBlockHashSubscription.unsubscribe();
+    var txOutSetInfoObservable = txOutSetInfoObservableCache[blockHash];
+    if (txOutSetInfoObservable == null)
+    {
+      txOutSetInfoObservable = Observable.create(function (observer) {
+
+        var txOutSetInfo = null;
+    
+        var getTxOutSetInfo = async () => {
+          var newTxOutSetInfo = await chaincoinService.chaincoinApi.getTxOutSetInfo(blockHash);
+    
+          if (txOutSetInfo != null && newTxOutSetInfo.height == txOutSetInfo.bestblock && newTxOutSetInfo.height == txOutSetInfo.bestblock) return;
+          txOutSetInfo = newTxOutSetInfo;
+          observer.next(newTxOutSetInfo);
+        };
+    
+        var bestBlockHashSubscription = chaincoinService.BestBlockHash.subscribe(bestBlockHash => getTxOutSetInfo());
+    
+        return () => {
+          bestBlockHashSubscription.unsubscribe();
+        }
+      }).pipe(shareReplay({
+        bufferSize: 1,
+        refCount: true
+      }));
+      
+      txOutSetInfoObservableCache[blockHash] = txOutSetInfoObservable;
     }
-  }).pipe(shareReplay({
-    bufferSize: 1,
-    refCount: true
-  }));
+
+    return txOutSetInfoObservable;
+  };
 };
