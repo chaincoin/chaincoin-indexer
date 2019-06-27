cb9bd88df68b36640ab6a975e3a7c56c0a445aac
chrismcna <chris.mcna@gmail.com>
2019-06-27 12:57:40 +0100

    Create PeerInfoObservable.js

---
+++ src/observables/PeerInfoObservable.js
@@ -0,0 +1,30 @@
+const { Observable, Subject } = require('rxjs');
+const { shareReplay } = require('rxjs/operators');
+
+
+module.exports = function (chaincoinService) {
+
+  return Observable.create(function (observer) {
+
+    var peerInfo = null;
+
+    var getPeerInfo = async () => {
+      var newPeerInfo = await chaincoinService.chaincoinApi.getPeerInfo();
+
+
+
+      if (peerInfo != null && JSON.stringify(newPeerInfo) == JSON.stringify(peerInfo)) return;
+      peerInfo = newPeerInfo;
+      observer.next(newPeerInfo);
+    };
+
+    var intervalId = setInterval(() => getPeerInfo(), 30000);
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  }).pipe(shareReplay({
+    bufferSize: 1,
+    refCount: true
+  }));
+};
