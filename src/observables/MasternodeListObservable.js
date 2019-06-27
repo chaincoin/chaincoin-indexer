61d10fbc7e9c7da42a791b55140ec38c9dd95a70
chrismcna <chris.mcna@gmail.com>
2019-06-27 12:57:35 +0100

    Create MasternodeListObservable.js

---
+++ src/observables/MasternodeListObservable.js
@@ -0,0 +1,28 @@
+const { Observable, Subject } = require('rxjs');
+const { shareReplay } = require('rxjs/operators');
+
+
+module.exports = function (chaincoinService) {
+
+  return Observable.create(function (observer) {
+
+    var masternodeList = null;
+
+    var getMasternodeList = async () => {
+      var newMasternodeList = await chaincoinService.chaincoinApi.getMasternodeList();
+
+      if (masternodeList != null && JSON.stringify(newMasternodeList) == JSON.stringify(masternodeList)) return;
+      masternodeList = newMasternodeList;
+      observer.next(newMasternodeList);
+    };
+
+    var intervalId = setInterval(() => getMasternodeList(), 30000);
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  }).pipe(shareReplay({
+    bufferSize: 1,
+    refCount: true
+  }));
+};
