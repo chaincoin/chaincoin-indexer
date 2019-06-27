105378707e8954adfc5b7bcfe0cec68586783387
chrismcna <chris.mcna@gmail.com>
2019-06-27 12:57:37 +0100

    Create MasternodeWinnersObservable.js

---
+++ src/observables/MasternodeWinnersObservable.js
@@ -0,0 +1,30 @@
+const { Observable, Subject } = require('rxjs');
+const { shareReplay } = require('rxjs/operators');
+
+
+module.exports = function (chaincoinService) {
+
+  return Observable.create(function (observer) {
+
+    var masternodeWinners = null;
+
+    var getMasternodeWinners = async () => {
+      var newMasternodeWinners = await chaincoinService.chaincoinApi.getMasternodeWinners();
+
+
+
+      if (masternodeWinners != null && JSON.stringify(newMasternodeWinners) == JSON.stringify(masternodeWinners)) return;
+      masternodeWinners = newMasternodeWinners;
+      observer.next(newMasternodeWinners);
+    };
+
+    var intervalId = setInterval(() => getMasternodeWinners(), 30000);
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  }).pipe(shareReplay({
+    bufferSize: 1,
+    refCount: true
+  }));
+};
