ac055c694735b2c6a987ff7cd3ddcaadb553a9e3
chrismcna <chris.mcna@gmail.com>
2019-06-27 13:53:08 +0100

    Create EstimateSmartFeeObservable.js

---
+++ src/observables/EstimateSmartFeeObservable.js
@@ -0,0 +1,46 @@
+const { Observable, Subject } = require('rxjs');
+const { shareReplay } = require('rxjs/operators');
+
+
+module.exports = function (chaincoinService) {
+
+};
+
+
+module.exports = function (chaincoinService) {
+
+  var estimateSmartFeeObservableCache = []; //TODO: memory leak
+
+  return (blockCount) => {
+
+    var estimateSmartFeeObservable = estimateSmartFeeObservableCache[blockCount];
+    if (estimateSmartFeeObservable == null)
+    {
+      estimateSmartFeeObservable = Observable.create(function (observer) {
+
+        var estimateSmartFee = null;
+    
+        var getTxOutSetInfo = async () => {
+          var newEstimateSmartFee = await chaincoinService.chaincoinApi.estimateSmartFee(blockCount);
+    
+          if (estimateSmartFee != null && JSON.stringify(newEstimateSmartFee) ==  JSON.stringify(estimateSmartFee)) return;
+          estimateSmartFee = newEstimateSmartFee;
+          observer.next(newEstimateSmartFee);
+        };
+    
+        var bestBlockHashSubscription = chaincoinService.BestBlockHash.subscribe(bestBlockHash => getTxOutSetInfo());
+    
+        return () => {
+          bestBlockHashSubscription.unsubscribe();
+        }
+      }).pipe(shareReplay({
+        bufferSize: 1,
+        refCount: true
+      }));
+      
+      estimateSmartFeeObservable[blockCount] = estimateSmartFeeObservable;
+    }
+
+    return estimateSmartFeeObservable;
+  };
+};
