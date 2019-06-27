1b2a196b68b80212574048784290f900bee2fa2f
chrismcna <chris.mcna@gmail.com>
2019-06-27 13:43:56 +0100

    Create BlockHashObservables.js

---
+++ src/observables/BlockHashObservables.js
@@ -0,0 +1,42 @@
+const { Observable, Subject } = require('rxjs');
+const { shareReplay } = require('rxjs/operators');
+
+
+module.exports = function (chaincoinService) {
+
+  var blockHashObservable = []; //TODO: memory leak
+
+  return (blockId) => {
+
+    var blockHashObservable = blockHashObservableCache[blockId];
+    if (blockHashObservable == null)
+    {
+      blockHashObservable = Observable.create(function (observer) {
+
+        var blockHash = null;
+    
+        var getBlock = async () => {
+          var newBlockHash = await chaincoinService.chaincoinApi.getBlockHash(blockId);
+    
+          if (blockHash != newBlockHash) return;
+          blockHash = newBlockHash;
+          observer.next(newBlockHash);
+        };
+    
+        getBlock();
+
+        //TODO: detect reorg and then make sure hash for blockId hasnt changed
+    
+        return () => {
+        }
+      }).pipe(shareReplay({
+        bufferSize: 1,
+        refCount: true
+      }));
+      
+      blockHashObservable[blockId] = blockHashObservable;
+    }
+
+    return blockObservable;
+  };
+};
