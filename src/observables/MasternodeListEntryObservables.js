048de421e8f8cee9065fee9e3a0b490f4f0fed6a
chrismcna <chris.mcna@gmail.com>
2019-06-30 18:03:22 +0100

    Create MasternodeListEntryObservables.js

---
+++ src/observables/MasternodeListEntryObservables.js
@@ -0,0 +1,38 @@
+const { Observable, Subject } = require('rxjs');
+const { shareReplay } = require('rxjs/operators');
+
+
+module.exports = function (chaincoinService) {
+
+  var masternodeListEntryObservableCache = []; //TODO: memory leak
+
+  return (output) => {
+
+    var masternodeListEntryObservable = masternodeListEntryObservableCache[output];
+    if (masternodeListEntryObservable == null)
+    {
+      masternodeListEntryObservable = Observable.create(function (observer) {
+
+        var masternode = null;
+    
+        var subscription = chaincoinService.MasternodeList.subscribe(masternodeList => {
+          var newMasternode = masternodeList[output];
+          if (JSON.stringify(masternode) == JSON.stringify(newMasternode)) return;
+          masternode = newMasternode;
+          observer.next(newMasternode);
+        });
+    
+        return () => {
+          subscription.unsubscribe();
+        }
+      }).pipe(shareReplay({
+        bufferSize: 1,
+        refCount: true
+      }));
+
+      masternodeListEntryObservableCache[output] = masternodeListEntryObservable;
+    }
+
+    return masternodeListEntryObservable;
+  };
+};
