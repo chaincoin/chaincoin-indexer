f094d92e67c297dbf6ab80f75e3b72b2c6012f49
chrismcna <chris.mcna@gmail.com>
2019-06-30 17:56:07 +0100

    Update ChainTxStatsObservables.js

--- src/observables/ChainTxStatsObservables.js
+++ src/observables/ChainTxStatsObservables.js
@@ -2,31 +2,6 @@ const { Observable, Subject } = require('rxjs');
 const { shareReplay } = require('rxjs/operators');
 
 
-module.exports = function (chaincoinService) {
-
-  return Observable.create(function (observer) {
-
-    var chainTxStats = null;
-
-    var getChainTxStats = async () => {
-      var newChainTxStats = await chaincoinService.chaincoinApi.getChainTxStats();
-
-      if (chainTxStats != null && newChainTxStats.time == chainTxStats.time) return;
-      chainTxStats = newChainTxStats;
-      observer.next(newChainTxStats);
-    };
-
-    var bestBlockHashSubscription = chaincoinService.BestBlockHash.subscribe(bestBlockHash => getChainTxStats());
-
-    return () => {
-      bestBlockHashSubscription.unsubscribe();
-    }
-  }).pipe(shareReplay({
-    bufferSize: 1,
-    refCount: true
-  }));
-};
-
 
 
 
@@ -66,6 +41,6 @@ module.exports = function (chaincoinService) {
       chainTxStatsObservableCache[blockHash] = chainTxStatsObservable;
     }
 
-    return txOutSetInfoObservable;
+    return chainTxStatsObservable;
   };
 };
