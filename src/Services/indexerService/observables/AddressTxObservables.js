ba1ab4c50e12c5c8ea1d447a1f6f185e4fdfd82d
chrismcna <chris.mcna@gmail.com>
2019-07-01 18:10:29 +0100

    Create AddressTxObservables.js

---
+++ src/Services/indexerService/observables/AddressTxObservables.js
@@ -0,0 +1,41 @@
+const { Observable, Subject } = require('rxjs');
+const { shareReplay } = require('rxjs/operators');
+
+
+module.exports = function (indexerService) {
+
+  var observableCache = {}; //TODO: memory leak
+
+  return (address, pos) => {
+
+    var observable = observableCache[address + "-" + pos];
+    if (observable == null)
+    {
+      
+      observable = Observable.create(function (observer) {
+
+        var addressTx = null;
+        var getAddressTx = async () =>{
+          var addressTx = await indexerService.indexApi.getAddressTx(address, pos);
+          observer.next(addressTx);
+        }
+    
+        var subscription = indexerService.AddressTx(address).subscribe(dbAddress => {
+          if (addressTx == null && dbAddress.txCount > pos) getAddressTx();
+        });
+
+
+        return () => {
+          subscription.unsubscribe();
+        }
+      }).pipe(shareReplay({
+        bufferSize: 1,
+        refCount: true
+      }));
+
+      observableCache[address] = observable;
+    }
+
+    return observable;
+  };
+};
