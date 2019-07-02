671240d15e5bf98900c291daffd9a1ff4f7a3ddb
chrismcna <chris.mcna@gmail.com>
2019-07-02 18:17:33 +0100

    Update BlockObservables.js

--- src/Services/chaincoinService/observables/BlockObservables.js
+++ src/Services/chaincoinService/observables/BlockObservables.js
@@ -1,51 +1,29 @@
-const { Observable, Subject } = require('rxjs');
-const { shareReplay } = require('rxjs/operators');
+const { Observable, Subject, from } = require('rxjs');
+const { shareReplay, switchMap, map } = require('rxjs/operators');
 
 
 module.exports = function (chaincoinService) {
 
-  var blockObservableCache = []; //TODO: memory leak
+  var blockCache = chaincoinService.BestBlockHash.pipe(
+    map(bestBlockHash => {
+      return {};
+    }),
+    shareReplay({
+      bufferSize: 1,
+      refCount: false
+    })
+  );
 
   return (hash) => {
 
-    var blockObservable = blockObservableCache[hash];
-    if (blockObservable == null)
-    {
-      blockObservable = Observable.create(function (observer) {
-
-        var block = null;
-    
-        var getBlock = async () => {
-
-          try
-          {
-            var newBlock = await chaincoinService.chaincoinApi.getBlock(hash);
-    
-            if (block != null && JSON.stringify(newBlock) == JSON.stringify(block)) return;
-            block = newBlock;
-          }
-          catch(ex)
-          {
-            observer.error(ex);
-          }
-
-          observer.next(newBlock);
-          
-        };
-    
-        var bestBlockHashSubscription = chaincoinService.BestBlockHash.subscribe(bestBlockHash => getBlock());
-    
-        return () => {
-          bestBlockHashSubscription.unsubscribe();
+    return blockCache.pipe(
+      switchMap(blockCache =>{
+        if (blockCache[hash] == null){
+          var promise = chaincoinService.chaincoinApi.getBlock(hash);
+          blockCache[hash] = promise;
         }
-      }).pipe(shareReplay({
-        bufferSize: 1,
-        refCount: true
-      }));
-
-      blockObservableCache[hash] = blockObservable;
-    }
-
-    return blockObservable;
+        return blockCache[hash];
+      })
+    )
   };
 };
