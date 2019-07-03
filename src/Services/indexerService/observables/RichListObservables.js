ac29e03def47242c904dcd47daa9396686b53463
chrismcna <chris.mcna@gmail.com>
2019-07-04 00:30:38 +0100

    Update RichListObservables.js

--- src/Services/indexerService/observables/RichListObservables.js
+++ src/Services/indexerService/observables/RichListObservables.js
@@ -1,20 +1,7 @@
-const { Observable, Subject } = require('rxjs');
+const { Observable, Subject, from } = require('rxjs');
 const { shareReplay } = require('rxjs/operators');
 
 
 module.exports = function (indexerService) { //TODO: not event driven or shared
-  return (pos,pageSize) => Observable.create(function (observer) {
-
-
-    var getRichList = async () =>{
-      var richList = await indexerService.indexApi.getRichList(pos,pageSize);
-      observer.next(richList);
-    }
-
-    getRichList();
-
-
-    return () => {
-    }
-  });
+  return (pos,pageSize) => from(indexerService.indexApi.getRichList(pos,pageSize));
 };
