bff04cfc9a982b808966e0c38020d4eeabae4ea9
chrismcna <chris.mcna@gmail.com>
2019-07-07 15:58:47 +0100

    Create MemPoolObservable.js

---
+++ src/Services/chaincoinService/observables/MemPoolObservable.js
@@ -0,0 +1,19 @@
+const  { Observable, Subject, combineLatest, of } = require('rxjs');
+const  { shareReplay, switchMap  } = require('rxjs/operators');
+
+  
+module.exports = function(chaincoinService){
+
+    return chaincoinService.RawMemPool.pipe( //TODO: this could be better, e.g. paging, subscribe to mempool filtered by address etc
+        switchMap(rawMemPool => {
+            if (rawMemPool.length == 0) return of([]);
+            return combineLatest(rawMemPool.map(txId =>chaincoinService.Transaction(txId)))
+        })
+    ).pipe(shareReplay({
+        bufferSize: 1,
+        refCount: true
+    }));
+    
+    
+    
+};
