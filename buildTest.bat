4841641881a144a927e517ecc1d22ff3914f6196
chrismcna <chris.mcna@gmail.com>
2019-07-07 16:23:36 +0100

    Update buildTest.bat

--- buildTest.bat
+++ buildTest.bat
@@ -1,3 +1,3 @@
-docker build -t mcna/images:chaincoin-index-v0.2-dev-ubuntu -f DockerFile ..\
+docker build -t mcna/images:chaincoin-index-v0.2-dev-ubuntu -f DockerFile .
 docker push mcna/images:chaincoin-index-v0.2-dev-ubuntu
 set /p temp="Hit enter to continue"
