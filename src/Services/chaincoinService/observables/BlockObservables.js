f0509f9d978e20230dbeb6318fc3385047814a25
chrismcna <chris.mcna@gmail.com>
2019-07-11 19:21:54 +0100

    Update BlockObservables.js

--- src/Services/chaincoinService/observables/BlockObservables.js
+++ src/Services/chaincoinService/observables/BlockObservables.js
@@ -1,31 +1,76 @@
-const { Observable, Subject, from } = require('rxjs');
-const { shareReplay, switchMap, map } = require('rxjs/operators');
+const { Observable, Subject, from, of } = require('rxjs');
+const { shareReplay, switchMap, map, filter, finalize } = require('rxjs/operators');
+const { refCountDelay } = require('rxjs-etc/operators');
 
 
 module.exports = function (chaincoinService) {
 
-  var blockCache = chaincoinService.BestBlockHash.pipe(
-    map(bestBlockHash => {
-      return {};
-    }),
-    shareReplay({
-      bufferSize: 1,
-      refCount: false
-    })
-  );
+
+  
+
+
+
+  var blockCache = {};
 
   return (hash) => {
 
-    return blockCache.pipe(
-      switchMap(blockCache =>{
-        if (blockCache[hash] == null){
-          var promise = chaincoinService.chaincoinApi.getBlock(hash);
-          blockCache[hash] = promise;
+    var observable = blockCache[hash];
+    if (observable == null)
+    {
+      var _block = null;
+      var updatingBlock = false;
 
-          promise.catch(() => blockCache[hash] = null);
-        }
-        return blockCache[hash];
-      })
-    )
+      observable = chaincoinService.BestBlockHash.pipe(
+        filter(bestBlockHash => {
+          return _block == null || _block.nextblockhash == null
+        }),
+        switchMap(bestBlockHash => {
+          var subject = new Subject();
+          updatingBlock = true;
+          chaincoinService.chaincoinApi.getBlock(hash)
+          .finally(()=>updatingBlock = false)
+          .then((block) => {
+            subject.next(block)
+          }).catch(err => {
+            subject.error(err)
+          });
+          return subject;
+        }),
+        switchMap(block =>{
+          _block = block;
+          if (block.nextblockhash != null){
+            return chaincoinService.BlockCount.pipe(
+              map(blockCount =>{
+                return Object.assign({},block,{confirmations:(blockCount - block.height) + 1});
+              })
+            );
+          } 
+          
+          return of(block);
+        }),
+        finalize(() => { 
+          delete blockCache[hash] 
+        }),
+        shareReplay({
+          bufferSize: 1,
+          refCount: true
+        }),
+        
+        filter(block => {
+          return !updatingBlock;
+        })
+        /*finalize(() => { 
+          var subscription = observable.subscribe(); 
+          setTimeout(() => {
+            subscription.unsubscribe()
+          },5000);
+        })*/
+      );
+      
+
+      blockCache[hash] = observable;
+    }
+    
+    return observable;
   };
 };
